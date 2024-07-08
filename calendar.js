adb4cdc93f75eb53aaedef7abbf23cf8367512bd
hyunjun <hjhm081031@gmail.com>
2024-07-09 01:00:33 +0900

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -70,10 +70,6 @@ const calendarCells = document.querySelectorAll('.calendar td');
 
 // 달력 날짜 클릭시
 calendarCells.forEach(cell => {
-    cell.addEventListener('click', function() {
-        // 여기에 이동할 URL을 지정
-        const url = 'https://example.com';
-        // 특정 URL로 이동합니다.
-        window.location.href = url;
+    cell.addEventListener('click', <button onclick="location.href='quetions.html'">자주 묻는 질문</button>
     });
 });
